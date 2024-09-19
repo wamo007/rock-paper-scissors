2418beb91a78a0a92030f1d80059715a6cfab8d0
Shamo <11028274+wamo007@users.noreply.github.com>
2024-09-19 19:44:02 +0200

    Rock Paper Scissors project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rock-paper-scissors
 Rock Paper Scissors for TOP
+
+It was a tough one. Somehow managed to get out in one piece.
+Used if-else statements, functions addressing global variables, while loop, for loop, etc.
+
+Made the messages in the form of alerts and prompts, so you would not need to go to console and type anything.
+But if you wanna take a peak through a console, be my guest!
---
+++ scripts.js
@@ -0,0 +1,63 @@
+console.log("Hello World!");
+
+let humanChoice = 0;
+let computerChoice = 0;
+
+//defining array values for the computer to randomly choose
+const options = ["rock", "paper", "scissors"];
+
+//using the array to return random choice
+function getComputerChoice(number = 3) {
+    return computerChoice = options[Math.floor(Math.random() * number)];
+}
+
+//while loop to reject other words.
+//Only our three words (regardless of the case) will be accepted
+function getHumanChoice(humanChoice = (prompt("Rock, Paper, Scissors, one, two, three!").toLowerCase())) {
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        alert("The choice is neither of them... Please choose rock, paper, or scissors.");
+        humanChoice = prompt("Rock, Paper, Scissors, one, two, three!").toLowerCase();
+    }
+    
+    return humanChoice;
+}
+
+//using score counter
+let humanScore = 0;
+let computerScore = 0;
+
+//Round begins. All the possible results are covered. 
+//Score counter adds 1 point for each win.
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        alert("Lost. Paper covers rock.");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        alert("Lost. Scissors cut paper.");
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        alert("Lost. Rock breaks scissors.");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        alert("You WON. Scissors cut paper.");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        alert("You WON. Paper covers rock.");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        alert("You WON. Rock breaks scissors.");
+    } else {
+        alert("That's a draw!");
+    }
+}
+
+//for loop to get the wheels turning for 5 times.
+for (let i = 0; i < 5; i++) {
+getComputerChoice(number=3);
+getHumanChoice(humanChoice = (prompt("Rock, Paper, Scissors, one, two, three!").toLowerCase()));
+playRound(humanChoice,computerChoice,humanScore,computerScore);
+alert(humanScore + ":" + computerScore);
+}
+
+alert("Currently, score for the humanity equals to " + humanScore + ". Score for terminators " + computerScore);
